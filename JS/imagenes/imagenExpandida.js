d423c2730990044fd2e4eb40902bc51a3ed34f9a
Esaul Alfacan <esaulalfacan@gmail.com>
2023-12-19 14:30:37 -0600

    Actualización de imágenes

--- JS/imagenes/imagenExpandida.js
+++ JS/imagenes/imagenExpandida.js
@@ -1,9 +1,13 @@
-document.querySelectorAll('.imagen-galeria').forEach(imagen => {
-    imagen.addEventListener('click', () => {
-        if (imagen.classList.contains('imagen-expandida')) {
-            imagen.classList.remove('imagen-expandida');
-        } else {
-            imagen.classList.add('imagen-expandida');
-        }
+document.querySelectorAll('.imagen-galeria').forEach(image => {
+    image.addEventListener('click', function () {
+        const previewContainer = document.getElementById('previewContainer');
+        const previewImage = document.getElementById('previewImage');
+
+        previewImage.src = this.src;
+        previewContainer.style.display = 'flex';
     });
+});
+
+document.getElementById('previewContainer').addEventListener('click', function () {
+    this.style.display = 'none';
 });
